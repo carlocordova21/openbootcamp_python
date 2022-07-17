852a7b0d506ae2bfc2ea1b4eeffa5d97225721d1
carlocordova21 <carlo_andre21@hotmail.com>
2022-07-17 18:24:37 -0500

    Ejercicio 6 completado

---
+++ ejercicio6/main.py
@@ -0,0 +1,8 @@
+def calcularAreaTriangulo(base, altura):
+    return (base * altura) / 2
+
+def calcularAreaCirculo(radio):
+    return 3.1416 * (radio ** 2)
+
+print(calcularAreaTriangulo(10, 20))
+print(calcularAreaCirculo(10))
